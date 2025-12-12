dccf7127f0f3303f6345a16c2ab3794bb86afcd6
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-12-12 21:29:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import cors from "cors";
-import aixRoute from "./routes/aix.js";
+import aixRouter from "./routes/aix.js";
 
 const app = express();
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// CORS FIX FOR RENDER
-app.use(cors({
-  origin: "*",
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type"
-}));
-
 app.get("/", (req, res) => {
-  res.json({ ok: true, msg: "Boss AIX Backend LIVE" });
+    res.json({ ok: true, msg: "Boss AIX Backend LIVE" });
 });
 
-app.use("/api/aix", aixRoute);
+app.use("/api/aix", aixRouter);
 
-app.listen(5000, () => {
-  console.log("🔥 Boss AIX Backend running on 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("🔥 Boss AIX Backend Running on PORT", PORT);
 });
