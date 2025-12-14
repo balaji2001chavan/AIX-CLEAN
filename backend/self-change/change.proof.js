f077f9a22e5afd45edcd47e9ea9c6fbb8c7b1beb
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-12-15 01:23:39 +0530

    Create change.proof.js

---
+++ backend/self-change/change.proof.js
@@ -0,0 +1,9 @@
+export function buildProof() {
+  return {
+    test: {
+      url: "/aix/command",
+      input: "घर खरेदी करावी का?",
+      expected: "मानवी, समजावून सांगणारे उत्तर"
+    }
+  };
+}
