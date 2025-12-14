6bff60afceed85869d6c856eb32bcc45e629a60d
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-12-15 01:38:28 +0530

    Update aix.routes.js

--- backend/routes/aix.routes.js
+++ backend/routes/aix.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { aixCommand } from "../controllers/aix.controller.js";
+import { approveChange } from "../approval/approval.controller.js";
 
+router.post("/approve-change", approveChange);
 const router = express.Router();
 
 router.post("/command", aixCommand);
