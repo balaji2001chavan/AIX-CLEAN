ad379ff3c9851c6ab9326d2a26843ee6db8ca096
balaji suryakant chavhan <balaji2001chavan@gmail.com>
2025-12-19 12:21:02 +0530

    Create toolRegistry.js

---
+++ backend/tools/toolRegistry.js
@@ -0,0 +1,12 @@
+export const AIX_TOOLS = [
+  {
+    name: "File Create",
+    command: "एक फाइल बनव",
+    description: "Server वर रियल फाइल तयार करतो"
+  },
+  {
+    name: "GitHub Commit",
+    command: "GitHub वर commit",
+    description: "Changes GitHub repo मध्ये push करतो"
+  }
+];
